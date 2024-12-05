35414331dd6541e768ff50a3be597d81265f8d42
BrennerGS <38071197+BrennerGS@users.noreply.github.com>
2024-12-05 01:19:51 -0300

    atualização link dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,22 @@ use App\Http\Middleware\CheckRoleCliente;
 use App\Http\Middleware\CheckRoleFotografo;
 use App\Http\Controllers\Foto_upload_controller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
 
-
-Route::resource('foto_upload', Foto_upload_controller::class);
-
-Route::middleware([CheckRoleCliente::class, 'auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+    Route::resource('foto_upload', Foto_upload_controller::class);
     
 });
 
 
-Route::get('/teste', function () {
-    return view('welcome');
-})->middleware([CheckRoleCliente::class]);
-
-Route::get('/teste', )->middleware([CheckRoleCliente::class]);
 
 
 
